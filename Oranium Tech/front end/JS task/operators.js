a065da63ea97ed04e4fb0a99ed3c599d90fb03ab
sága <the.people.stalking.me@gmail.com>
2024-10-19 10:33:34 +0530

    folder rename + reorder

---
+++ Oranium Tech/front end/JS task/operators.js
@@ -0,0 +1,43 @@
+
+// Arithmetic Operators
+
+let item_1= 20;
+let item_2 = 15;
+let item_3 = 10;
+console.log( "Total price = "item_1 + item_2 + item_3);
+
+// ---------------------------------------------
+
+
+// Relational Operators
+
+
+// ---------------------------------------------
+
+
+// Logical Operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
