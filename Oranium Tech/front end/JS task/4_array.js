a8dda8ab1002f1ffd031acd69fbde5a7fcc71cbc
username-8 <the.people.stalking.me@gmail.com>
2024-11-02 11:52:26 +0530

    array problem

--- Oranium Tech/front end/JS task/4_array.js
+++ Oranium Tech/front end/JS task/4_array.js
@@ -1,3 +1,10 @@
-// Given an array of numbers, write a function using map that returns a new array where every number is squared.
-// let numbers = [2, 4, 6, 8];
+//1. Square Every Number:  Given an array of numbers, write a function using map that returns a new array where every number is squared.
 // Expected Output: [4, 16, 36, 64]
+
+let numbers = [2, 4, 6, 8];
+let numsqare = numbers.map(square)
+
+function square(num) {
+    return num * num;
+}
+console.log(numsqare);
